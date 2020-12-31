8b4c4926fc01c497d267fd827919c4c8af2730e5
Aleksa Majkic <aleksamcode@gmail.com>
2020-12-31 12:35:11 +0100

    New class EnigmaEFS used to create, modify or manipulate an Enigma Encripted File System.

---
+++ Enigma/EFS/EnigmaEFS.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Enigma
+{
+    enum EnigmaEFSError
+    {
+        MountLocationDoesntExist,
+        DriveNotFound,
+        DirectoryNotFound,
+        InsufficientStorageAvailable,
+        WrongFileType,
+        FilePermission
+    }
+
+    /// <summary>
+    /// Allows to create, modify or manipulate an Enigma Encripted File System.
+    /// </summary>
+    public class EnigmaEFS
+    {
+        private readonly string mountLocation = @"D:";
+        private readonly string rootDir = @"D:\EnigmaEFS";
+        private readonly string sharedDir = @"D:\EnigmaEFS\Shared";
+        public long AvailableFreeSpace { get; set; }
+        public EnigmaEFS()
+        {
+            // EFS "mount"
+            if (Directory.Exists(mountLocation))
+            {
+                // create a new root directory if one isn't already created
+                if (!Directory.Exists(mountLocation))
+                {
+                    Directory.CreateDirectory(rootDir);
+                }
+                // create a new shared directory if one isn't already created
+                else if (Directory.Exists(mountLocation) && !Directory.Exists(mountLocation))
+                {
+                    Directory.CreateDirectory(sharedDir);
+                }
+            }
+            else
+            {
+                // error handle
+            }
+            // set AvailableFreeSpace value
+            AvailableFreeSpace = new DriveInfo(mountLocation).AvailableFreeSpace;
+        }
+
+        public bool CanItBeStored(long size)
+        {
+            AvailableFreeSpace = new DriveInfo(mountLocation).AvailableFreeSpace;
+            if (size < AvailableFreeSpace)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
