72102731d4dbe476c44786cbf79378e3ca178fb2
Aleksa Majkic <aleksamcode@gmail.com>
2020-12-31 20:47:32 +0100

    Layout of the STANDARD_INFORMATION attribute created in a class StandardInformation.

---
+++ Enigma/EFS/Attributes/StandardInformation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma
+{
+    public class StandardInformation : Attribute
+    {
+        public DateTime CreationTime { get; set; }
+        public uint CTimeUsedId { get; set; }
+        /// <summary>
+        /// Last modification date and time (also referred to as last written date and time).
+        /// </summary>
+        public DateTime AlteredTime { get; set; }
+        public uint ATimeUserId { get; set; }
+        /// <summary>
+        /// Last access time.
+        /// </summary>
+        public DateTime ReadTime { get; set; }
+        public uint RTimeUserId { get; set; }
+        public uint OwnerId { get; set; }
+    }
+}
