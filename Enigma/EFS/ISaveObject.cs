8272172c4a404e0c3291308256e114069bfa0fbb
Aleksa Majkic <aleksamcode@gmail.com>
2020-12-31 13:51:28 +0100

    Added new interface ISaveObjects used to store and save encrypted data.

---
+++ Enigma/EFS/ISaveObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma
+{
+    public interface ISaveObjects
+    {
+        int GetSaveLength();
+        void Save(byte[] buffer, int offset);
+    }
+}
