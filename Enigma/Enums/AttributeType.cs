92217cb7ac16afbebfd2e55b3340ff3173ccf53a
Aleksa Majkic <aleksamcode@gmail.com>
2020-12-31 13:18:24 +0100

    New enum AttributeType used to represent different headers in a encrypted file.

---
+++ Enigma/Enums/AttributeType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma
+{
+    public enum AttributeType : uint
+    {
+        Unkown = 0,
+        STANDARD_INFORMATION = 0x10,
+        FILE_NAME = 0x30,
+        SECURITY_DESCRIPTOR = 0x50,
+        DATA = 0x80,
+        EndOfAttributes = uint.MaxValue
+    }
+}
