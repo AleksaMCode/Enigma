16f1f7da0edf9a1a710ec77c652127810af181e0
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-01 20:18:14 +0100

    New interface IAlgorithm added.

---
+++ Enigma/AlgorithmLibrary/Algorithms/Interface1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma
+{
+    /// <summary>
+    /// Interface for implementation all encryption algorithms.
+    /// </summary>
+    public interface IAlgorithm
+    {
+        int BlockSize { get; }
+        byte[] Key { get; }
+        byte[] AdditionalData { get; }
+        byte[] Encrypt(byte[] message);
+        byte[] Decrypt(byte[] message);
+        string GetSignatureString();
+    }
+}
