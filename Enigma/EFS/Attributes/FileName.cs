d87e08793f6875a034dfb1368deb8a75e379f8d9
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-01 19:02:33 +0100

    Layout of the FILE_NAME attribute created in a class FileName.

---
+++ Enigma/EFS/Attributes/FileName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma
+{
+    public class FileName : Attribute
+    {
+        public string ParentDirectory { get; set; }
+        public ulong FileSize { get; set; }
+        public string RealFileName { get; set; }
+    }
+}
