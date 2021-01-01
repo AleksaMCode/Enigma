0a127d70f44869b3ff41cbbcd8b96a1b615320c5
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-02 00:32:30 +0100

    New RsaAlgorithm class added. Implementation of RSA algorithm.

---
+++ Enigma/AlgorithmLibrary/Algorithms/RsaAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.AlgorithmLibrary.Algorithms
+{
+    public class RsaAlgorithm : IAlgorithm
+    {
+        public static readonly string NameSignature = "RSA";
+        public RSAParameters Key { get; set; }
+        byte[] IAlgorithm.Key => null;
+        public int BlockSize { get; } = 115;
+        public byte[] AdditionalData { get => null; }
+
+        public RsaAlgorithm(RSAParameters rsaKeyParams)
+        {
+            Key = rsaKeyParams;
+        }
+    }
+}
