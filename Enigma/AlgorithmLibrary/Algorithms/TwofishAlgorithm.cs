ae325d43dfb4b0472bda5cb9ace68dfaef19cd5e
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-02 22:08:25 +0100

    New TwofishAlgorithm class added. Implementation of Twofish algorithm.

---
+++ Enigma/AlgorithmLibrary/Algorithms/TwofishAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.AlgorithmLibrary.Algorithms
+{
+    public class TwofishAlgorithm : IAlgorithm
+    {
+        public static readonly string NameSignature = "2fish";
+
+        public static string ModeSignature = null;
+
+        /// <summary>
+        /// Twofish accepts a key of any length up to 256 bits. (NIST required the algorithm to accept 128-, 192-, and 256-bit keys.) 
+        /// </summary>
+        public byte[] Key { get; set; }
+
+        /// <summary>
+        /// Twofish has a block size of 128 bits.
+        /// </summary>        
+        public byte[] IV { get; set; }
+
+        public byte[] AdditionalData { get => this.IV; }
+    }
+}
