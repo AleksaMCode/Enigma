48e286b467e8a98b4768e10f8a5cc214bb3e097e
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-02 16:49:47 +0100

    Constructor added to TripleDesAlgorithm class.

--- Enigma/AlgorithmLibrary/Algorithms/TripleDesAlgorithm.cs
+++ Enigma/AlgorithmLibrary/Algorithms/TripleDesAlgorithm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,8 +10,25 @@ namespace Enigma.AlgorithmLibrary.Algorithms
     public class TripleDesAlgorithm : IAlgorithm
     {
         public static readonly string NameSignature = "3DES";
+
         public byte[] Key { get; set; }
+
         public byte[] IV { get; set; }
+
         public byte[] AdditionalData { get => this.IV; }
+
+        public TripleDesAlgorithm()
+        {
+            Key = new byte[24];
+            new RNGCryptoServiceProvider().GetBytes(Key);
+            IV = new byte[16];
+            new RNGCryptoServiceProvider().GetBytes(IV);
+        }
+
+        public TripleDesAlgorithm(byte[] key, byte[] iv)
+        {
+            Key = key;
+            IV = iv;
+        }
     }
 }
