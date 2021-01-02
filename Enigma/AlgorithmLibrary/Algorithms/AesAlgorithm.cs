28d0aa49bf03da2997f47c25684a4c5da939685d
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-02 21:10:12 +0100

    New AesAlgorithm class added. Implementation of AES algorithm.

---
+++ Enigma/AlgorithmLibrary/Algorithms/AesAlgorithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.AlgorithmLibrary.Algorithms
+{
+    public class AesAlgorithm : IAlgorithm
+    {
+        public static readonly string signature = "AES";
+        public byte[] Key { get; set; }
+        public byte[] IV { get; set; }
+    }
+}
