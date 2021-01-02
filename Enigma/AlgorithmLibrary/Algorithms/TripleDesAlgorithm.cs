388b6d6bd2d26bed89a502c7115429ea41927647
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-02 14:28:46 +0100

    New TripleDesAlgorithm class added. Implementation of 3DSE algorithm.

---
+++ Enigma/AlgorithmLibrary/Algorithms/TripleDesAlgorithm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.AlgorithmLibrary.Algorithms
+{
+    public class TripleDesAlgorithm : IAlgorithm
+    {
+        public static readonly string NameSignature = "3DES";
+        public byte[] Key { get; set; }
+        public byte[] IV { get; set; }
+        public byte[] AdditionalData { get => this.IV; }
+    }
+}
