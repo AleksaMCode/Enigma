fd934ef4b34f971186de93ad9c42bea849aa144b
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-03 17:27:04 +0100

    RSA key exceptions added.

---
+++ Enigma/PrivateKeyParsers/InvalidKeyFileException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Enigma
+{
+    public class InvalidKeyFileException : Exception
+    {
+        public InvalidKeyFileException()
+        {
+        }
+
+        public InvalidKeyFileException(string message) : base(message)
+        {
+        }
+
+        public InvalidKeyFileException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidKeyFileException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
