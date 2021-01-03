131526b85301d57e68cb75bfbce63d22cdc4d2ed
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-03 15:09:06 +0100

    New CamelliaAlgorithm class added. Implementation of Camellia algorithm.

---
+++ Enigma/AlgorithmLibrary/Algorithms/CamelliaAlgorithm.cs
@@ -0,0 +1,26 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+using System.Security.Cryptography;
+
+namespace Enigma
+{
+    /// <summary>
+    /// Camellia specifies the 128-bit block size and 128-, 192-, and 256-bit key sizes, the same interface as the Advanced Encryption Standard (AES).
+    /// </summary>
+    public class CamelliaAlgorithm : IAlgorithm
+    {
+        public static readonly string NameSignature = "CAMLL";
+
+
+        public static string ModeSignature = null;
+
+
+        public byte[] Key { get; set; }
+  
+
+        public byte[] IV { get; set; }
+    }
+}
