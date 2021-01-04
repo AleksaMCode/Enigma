0fcae299b28c54fde2d6314e0d86619de8ad87a1
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-04 15:47:22 +0100

    New LoginControler class added.

---
+++ Enigma/EnigmaWpf/Models/LoginControler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Enigma
+{
+    public class LoginControler
+    {
+        private readonly string privateKeyPath;
+        private readonly string userDatabasePath = @"C:\Users\Aleksa\source\repos\Enigma\Enigma\Users.db";
+
+        public UserInformation Login(string username, string password, out UserDatabase data)
+        {
+            UserDatabase dataComp = new UserDatabase(this.userDatabasePath);
+
+            var user = dataComp.GetUser(username);
+
+            if (user != null && user.IsPasswordValid(password))
+            {
+                var userCert = new X509Certificate2(user.PublicCertificate);
+
+                if (userCert == null)
+                {
+                    throw new Exception("Certificate error.");
+                }
+
+                if (CertificateValidator.VerifyCertificate(userCert) == false)
+                {
+                    throw new Exception("Certificate is invalid.");
+                }
+            }
+            else
+            {
+                throw new Exception("Invalid username or password.");
+            }
+
+            data = dataComp;
+            return new UserInformation(user);
+        }
+    }
+}
