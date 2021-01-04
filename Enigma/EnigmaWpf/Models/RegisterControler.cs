e64ed432dd0799afe9cc0621af80321bf782e6fb
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-04 09:35:47 +0100

    New RegisterControler class aded.

---
+++ Enigma/EnigmaWpf/Models/RegisterControler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+
+namespace Enigma
+{
+    public class RegisterControler
+    {
+        private readonly UserDatabase data;
+
+        public RegisterControler(UserDatabase db)
+        {
+            data = db;
+        }
+
+        internal void Register(string username, string password, string certificateFilePath)
+        {
+            X509Certificate2 cert = new X509Certificate2(certificateFilePath);
+
+            if (CertificateValidator.VerifyCertificate(cert) == false)
+            {
+                throw new Exception("Certificate is invalid.");
+            }
+            else if (CertificateValidator.VerifyKeyUsage(cert) == false)
+            {
+                throw new Exception("Certificate must have 'digitalSignature' and 'keyEncipherment' set as it's key usage.");
+            }
+
+            this.data.AddUser(username, password, File.ReadAllBytes(certificateFilePath));
+        }
+    }
+}
