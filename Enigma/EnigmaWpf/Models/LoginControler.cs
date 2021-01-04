87a2d4c8042b201c78c35528d74dedcbecc0af41
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-04 15:57:11 +0100

    LoginPartTwo method added for the 2FA.

--- Enigma/EnigmaWpf/Models/LoginControler.cs
+++ Enigma/EnigmaWpf/Models/LoginControler.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Enigma
 {
     public class LoginControler
     {
-        private readonly string privateKeyPath;
+        private string privateKeyPath;
         private readonly string userDatabasePath = @"C:\Users\Aleksa\source\repos\Enigma\Enigma\Users.db";
 
-        public UserInformation Login(string username, string password, out UserDatabase data)
+        public UserInformation LoginPartOne(string username, string password, out UserDatabase data)
         {
             UserDatabase dataComp = new UserDatabase(this.userDatabasePath);
 
@@ -35,6 +37,23 @@ namespace Enigma
 
             data = dataComp;
             return new UserInformation(user);
+        }
+
+        public void LoginPartTwo(string privateKeyPath, ref UserInformation user)
+        {
+            this.privateKeyPath = privateKeyPath;
+            byte[] keyRawEncrypted = File.ReadAllBytes(this.privateKeyPath);
+            
+            // TODO: decrypt the raw key file and create keyRaw
+
+            byte[] keyRaw;
+            var privateParameters = new KeyFileParser(keyRaw).GetParameters();
+            RSACryptoServiceProvider publicKeyProvider = (RSACryptoServiceProvider)user.Certificate.PublicKey.Key;
+            
+            if (!RsaAlgorithm.AreKeysMatched(publicKeyProvider.ExportParameters(false), privateParameters))
+            {
+                throw new Exception("The given private key does not match this user's certificate.");
+            }
         }
     }
 }
