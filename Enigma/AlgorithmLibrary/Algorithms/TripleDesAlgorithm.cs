047d01888e5461ef6a844e9a740f8796093b2444
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-07 13:36:19 +0100

    OFB mode added to 3DES using BouncyCastle.

--- Enigma/AlgorithmLibrary/Algorithms/TripleDesAlgorithm.cs
+++ Enigma/AlgorithmLibrary/Algorithms/TripleDesAlgorithm.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using Org.BouncyCastle.Crypto;
 using System.Security.Cryptography;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Parameters;
 
 namespace Enigma
 {
     public class TripleDesAlgorithm : IAlgorithm
     {
         public static readonly string NameSignature = "3DES";
-
 
         public static string ModeSignature = null;
 
@@ -43,33 +46,102 @@ namespace Enigma
             ModeSignature = mode;
         }
 
+        public IBufferedCipher CreateTripleDesCipher(bool forEncryption)
+        {
+            IBufferedCipher cipher;
+            var keyParameter = new KeyParameter(Key);
+            var keyWithIv = new ParametersWithIV(keyParameter, IV);
+
+            switch (ModeSignature)
+            {
+                case "OFB":
+                    {
+                        cipher = new BufferedBlockCipher(new OfbBlockCipher(new DesEdeEngine(), 8)); // DesEdeEngine or Triple DES engine
+                        cipher.Init(forEncryption, keyWithIv);
+                        return cipher;
+                    }
+                default:
+                    {
+                        throw new UnknownCipherModeException(ModeSignature);
+                    }
+            }
+        }
+
         public byte[] Encrypt(byte[] data)
         {
-            using TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
-            tdes.Mode = AlgorithmUtility.GetCipherMode(ModeSignature);
-            //tdes.Padding = PaddingMode.PKCS7;
-            using ICryptoTransform encryptor = tdes.CreateEncryptor(Key, IV);
-            using MemoryStream ms = new MemoryStream();
-            using CryptoStream writer = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+            if (!ModeSignature.Equals("OFB"))
+            {
+                using TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+                tdes.Mode = AlgorithmUtility.GetCipherMode(ModeSignature);
+                //tdes.Padding = PaddingMode.PKCS7;
+                using ICryptoTransform encryptor = tdes.CreateEncryptor(Key, IV);
+                using MemoryStream ms = new MemoryStream();
+                using CryptoStream writer = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
 
-            writer.Write(data, 0, data.Length);
-            writer.FlushFinalBlock();
-            return ms.ToArray();
+                writer.Write(data, 0, data.Length);
+                writer.FlushFinalBlock();
+                return ms.ToArray();
+            }
+            else
+            {
+                byte[] encrypted;
+                var tdes = CreateTripleDesCipher(true);
+
+                try
+                {
+                    byte[] inData = data;
+                    encrypted = new byte[tdes.GetOutputSize(inData.Length)];
+
+                    int len = tdes.ProcessBytes(inData, 0, inData.Length, encrypted, 0);
+                    tdes.DoFinal(encrypted, len);
+
+                    return encrypted;
+                }
+                catch (CryptoException)
+                {
+                }
+
+            }
+            return null;
         }
 
         public byte[] Decrypt(byte[] data)
         {
-            using TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
-            tdes.Mode = AlgorithmUtility.GetCipherMode(ModeSignature);
+            if (!ModeSignature.Equals("OFB"))
+            {
+                using TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+                tdes.Mode = AlgorithmUtility.GetCipherMode(ModeSignature);
 
-            using ICryptoTransform decryptor = tdes.CreateDecryptor(Key, IV);
-            using MemoryStream ms = new MemoryStream(data);
-            using CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+                using ICryptoTransform decryptor = tdes.CreateDecryptor(Key, IV);
+                using MemoryStream ms = new MemoryStream(data);
+                using CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
 
-            var decrypted = new byte[data.Length];
-            var bytesRead = cs.Read(decrypted, 0, data.Length);
+                var decrypted = new byte[data.Length];
+                var bytesRead = cs.Read(decrypted, 0, data.Length);
 
-            return decrypted.Take(bytesRead).ToArray();
+                return decrypted.Take(bytesRead).ToArray();
+            }
+            else
+            {
+                byte[] decrypted;
+                var tdes = CreateTripleDesCipher(false);
+
+                try
+                {
+                    byte[] inData = data;
+                    decrypted = new byte[tdes.GetOutputSize(inData.Length)];
+
+                    int len = tdes.ProcessBytes(inData, 0, inData.Length, decrypted, 0);
+                    tdes.DoFinal(decrypted, len);
+
+                    return decrypted;
+                }
+                catch (CryptoException)
+                {
+                }
+            }
+
+            return null;
         }
 
         public string GetAlgorithmNameSignature()
