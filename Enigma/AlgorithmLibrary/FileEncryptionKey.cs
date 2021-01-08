3f0e186ed0ec4277ff30bb2352b3592d5a3436b1
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-08 12:56:53 +0100

    Code refactoring +small update.

--- Enigma/AlgorithmLibrary/FileEncryptionKey.cs
+++ Enigma/AlgorithmLibrary/FileEncryptionKey.cs
@@ -1,31 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Enigma
+﻿namespace Enigma
 {
     /// <summary>
-    /// EnigmaEFS works by encrypting a file with a unique symmetric key (Key + IV/Salt), also known as the File Encryption Key, or FEK.
+    /// EnigmaEFS works by encrypting a file with a unique symmetric key (Key + IV), also known as the File Encryption Key, or FEK.
     /// It uses a symmetric encryption algorithm because it takes less time to encrypt and decrypt large amounts of data than if an asymmetric key cipher is used.
-    /// The symmetric encryption algorithm used will vary depending on the users choice.
-    /// The FEK (the symmetric key that is used to encrypt the file) is then encrypted with a public key that is associated with the user who encrypted the file, 
+    /// The symmetric encryption algorithm used will vary depending on the users choice. FEK also contains hashing algorithm that user chose to sign his file.
+    /// FEK is then encrypted with a public key that is associated with the user who encrypted the file, 
     /// and this encrypted FEK is stored in the EFS part of the SECURITY_DESCRIPTOR header of the encrypted file.
     /// </summary>
     public class FileEncryptionKey
     {
         public string AlgorithmName { get; set; }
 
-        public string AlgorithmMode { get; set; }
-        
+        public string HashAlgorithmName { get; set; }
+
         public byte[] Key { get; set; }
 
         public byte[] IV { get; set; }
 
-        public FileEncryptionKey(string algoName, IAlgorithm algo)
+        public FileEncryptionKey(string algoName, string hashName)
         {
-            //TODO: complete
+            AlgorithmName = algoName;
+            HashAlgorithmName = hashName;
         }
     }
 }
