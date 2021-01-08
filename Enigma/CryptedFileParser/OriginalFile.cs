cbb2be4e62ce016bb590b0a40d40a9006433a42d
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-08 13:11:20 +0100

    OriginalFile class added.

---
+++ Enigma/CryptedFileParser/OriginalFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Enigma
+{
+    public class OriginalFile
+    {
+        public Stream FileContent { get; internal set; }
+
+        public string FileExtension { get; internal set; }
+
+        /// <summary>
+        /// List of allowed file extensions.
+        /// </summary>
+        private string[] allowedExtensions = { "txt", "docx", "png", "jpeg", "pdf" };
+
+        public OriginalFile(Stream fileContent, string fileExtension)
+        {
+            ExtensionCheck(fileExtension);
+            FileContent = fileContent;
+            FileExtension = fileExtension;
+        }
+
+        public bool ExtensionCheck(string fileExtension)
+        {
+            foreach(string extension in allowedExtensions)
+            {
+                if(fileExtension.Equals(extension))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
