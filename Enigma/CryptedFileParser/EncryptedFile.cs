fda11f3c0d797c22175f87da7d576bd24c7c56a9
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-10 17:56:16 +0100

    EncryptedFile class added.

---
+++ Enigma/CryptedFileParser/EncryptedFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Enigma
+{
+    public class EncryptedFile
+    {
+        internal readonly Stream EncrypteFileContent;
+
+        /// <summary>
+        /// Users public key is used for name encryption.
+        /// </summary>
+        public string EncriptedName { get; internal set; }
+
+        public readonly string fileExtension = "at";
+    }
+}
