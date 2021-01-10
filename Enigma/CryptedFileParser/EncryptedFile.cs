5af416ea9bd62477e4d6dbb0f7da7199795acb03
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-10 18:19:29 +0100

    NameDecryption added  + constructor changed.

--- Enigma/CryptedFileParser/EncryptedFile.cs
+++ Enigma/CryptedFileParser/EncryptedFile.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.IO;
-using System.Security.Cryptography;
+﻿using System.IO;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace Enigma
 {
@@ -12,25 +11,42 @@ namespace Enigma
         /// <summary>
         /// Users public key is used for name encryption.
         /// </summary>
-        public string EncriptedName { get; internal set; }
+        public string EncriptedName { get; internal set; } = null;
 
         public readonly string fileExtension = "at";
 
-        internal EncryptedFile(Stream encrypteFileContent, string name, RSAParameters publicKey)
+        internal EncryptedFile(Stream encrypteFileContent)
         {
             encrypteFileContent.Position = 0;
             EncrypteFileContent = encrypteFileContent;
-            NameEncryption(name, publicKey);
         }
 
-        private void NameEncryption(string name, RSAParameters publicKey)
+        public void NameEncryption(string name, RSAParameters publicKey)
         {
             byte[] originalNameArray = Encoding.ASCII.GetBytes(name);
-            
+
             using RSACryptoServiceProvider encryptRSA = new RSACryptoServiceProvider();
             encryptRSA.ImportParameters(publicKey);
             byte[] encriptedNameArray = encryptRSA.Encrypt(originalNameArray, false);
+
             EncriptedName = Encoding.ASCII.GetString(encriptedNameArray);
+        }
+
+        public bool NameDecryption(RSAParameters privateKey, ref string name)
+        {
+            if (EncriptedName == null)
+            {
+                return false;
+            }
+
+            byte[] encriptedNameArray = Encoding.ASCII.GetBytes(EncriptedName);
+
+            using RSACryptoServiceProvider decryptRSA = new RSACryptoServiceProvider();
+            decryptRSA.ImportParameters(privateKey);
+            byte[] originalNameArray = decryptRSA.Decrypt(encriptedNameArray, false);
+
+            name = Encoding.ASCII.GetString(originalNameArray);
+            return true;
         }
     }
 }
