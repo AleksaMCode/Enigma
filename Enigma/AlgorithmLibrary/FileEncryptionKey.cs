48f21322d300bd7c5aa37e211621b45268cea6cc
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-12 15:35:07 +0100

    DecryptKeyAndIvBlock/EncryptKeyAndIvBlock methods added ; constructor update + new contructor added; ParseAlgorithNameSignature and ParseKeyAndIv methods added.

--- Enigma/AlgorithmLibrary/FileEncryptionKey.cs
+++ Enigma/AlgorithmLibrary/FileEncryptionKey.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Security.Cryptography;
+using Enigma.AlgorithmLibrary.Algorithms;
+
 namespace Enigma.AlgorithmLibrary
 {
     /// <summary>
@@ -9,18 +13,72 @@ namespace Enigma.AlgorithmLibrary
     /// </summary>
     public class FileEncryptionKey
     {
+        /// <summary>
+        /// Name of the algorithm used for file encryption/decryption.
+        /// </summary>
         public string AlgorithmName { get; set; }
 
+        /// <summary>
+        /// Key size stored as string.
+        /// </summary>
+        public string AlgorithmKeySize { get; set; }
+
+        /// <summary>
+        /// Block cipher mode of operation that the block cipher uses.
+        /// </summary>
+        public string ModeOfOperationName { get; set; }
+
+        /// <summary>
+        /// Name of the hashing algorithm used to sign original file.
+        /// </summary>
         public string HashAlgorithmName { get; set; }
 
         public byte[] Key { get; set; }
 
         public byte[] IV { get; set; }
 
-        public FileEncryptionKey(string algoName, string hashName)
+        public FileEncryptionKey(string algorithmNameSignature, string hashAlgorithm, byte[] key, byte[] iv)
         {
-            AlgorithmName = algoName;
-            HashAlgorithmName = hashName;
+            ParseAlgorithNameSignature(algorithmNameSignature);
+            HashAlgorithmName = hashAlgorithm;
+            Key = key;
+            IV = iv;
+        }
+
+        public FileEncryptionKey(string algorithmNameSignature, string hashAlgorithm, byte[] keyAndIvEncryptedBlock, RSAParameters privateKey)
+        {
+            ParseAlgorithNameSignature(algorithmNameSignature);
+            HashAlgorithmName = hashAlgorithm;
+            DecryptKeyAndIvBlock(keyAndIvEncryptedBlock, privateKey);
+        }
+
+        private void ParseAlgorithNameSignature(string algorithmNameSignature)
+        {
+            var tokens = algorithmNameSignature.Split('-');
+            AlgorithmName = tokens[0];
+            AlgorithmKeySize = tokens[1];
+            ModeOfOperationName = tokens[2];
+        }
+
+        private void ParseKeyAndIv(byte[] keyAndIvBlock)
+        {
+            Key = new byte[GetKeySizeFromAlgoName(AlgorithmName)];
+            IV = new byte[GetIvSizeFromAlgoName(AlgorithmName, AlgorithmKeySize)];
+
+            Buffer.BlockCopy(keyAndIvBlock, 0, Key, 0, Key.Length);
+            Buffer.BlockCopy(keyAndIvBlock, Key.Length, IV, 0, IV.Length);
+        }
+
+        private void DecryptKeyAndIvBlock(byte[] keyAndIvEncryptedBlock, RSAParameters privateKey)
+        {
+            var decryptor = new RsaAlgorithm(privateKey);
+            ParseKeyAndIv(decryptor.Decrypt(keyAndIvEncryptedBlock));
+        }
+
+        public byte[] EncryptKeyAndIvBlock(byte[] keyAndIvBlock, RSAParameters publicKey)
+        {
+            var encryptor = new RsaAlgorithm(publicKey);
+            return encryptor.Encrypt(keyAndIvBlock);
         }
     }
 }
