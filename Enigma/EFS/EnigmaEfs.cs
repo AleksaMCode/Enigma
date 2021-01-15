c03085cf3b44cfedf597f6169d9f56d3b42f6a9f
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-15 14:29:40 +0100

    Class name updated.

---
+++ Enigma/EFS/EnigmaEfs.cs
@@ -0,0 +1,51 @@
+using System.IO;
+
+namespace Enigma.EFS
+{
+    /// <summary>
+    /// Allows to create, modify or manipulate an Enigma Encripted File System.
+    /// </summary>
+    public class EnigmaEfs
+    {
+        private readonly string mountLocation = @"D:";
+        private readonly string rootDir = @"D:\EnigmaEFS";
+        private readonly string sharedDir = @"D:\EnigmaEFS\Shared";
+        public long AvailableFreeSpace { get; set; }
+        public EnigmaEfs()
+        {
+            // EFS "mount"
+            if (Directory.Exists(mountLocation))
+            {
+                // create a new root directory if one isn't already created
+                if (!Directory.Exists(mountLocation))
+                {
+                    Directory.CreateDirectory(rootDir);
+                }
+                // create a new shared directory if one isn't already created
+                else if (Directory.Exists(mountLocation) && !Directory.Exists(mountLocation))
+                {
+                    Directory.CreateDirectory(sharedDir);
+                }
+            }
+            else
+            {
+                // error handle
+            }
+            // set AvailableFreeSpace value
+            AvailableFreeSpace = new DriveInfo(mountLocation).AvailableFreeSpace;
+        }
+
+        public bool CanItBeStored(long size)
+        {
+            AvailableFreeSpace = new DriveInfo(mountLocation).AvailableFreeSpace;
+            if (size < AvailableFreeSpace)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
