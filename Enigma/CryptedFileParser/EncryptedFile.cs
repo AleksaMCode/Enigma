e5deb638548e68d95bed362c54312036115ce7a3
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-15 21:04:45 +0100

    Bug fixed.

--- Enigma/CryptedFileParser/EncryptedFile.cs
+++ Enigma/CryptedFileParser/EncryptedFile.cs
@@ -1,8 +1,8 @@
-using System;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using Enigma.AlgorithmLibrary.Algorithms;
+using Enigma.EFS.Attributes;
 
 namespace Enigma.CryptedFileParser
 {
