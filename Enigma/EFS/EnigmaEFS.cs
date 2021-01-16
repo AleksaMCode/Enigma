23bf4e34615146a08a6ea5c84fffe2a007b31d97
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-16 19:19:33 +0100

    Upload/Downlod methods added to EnigmaEfs class.

--- Enigma/EFS/EnigmaEFS.cs
+++ Enigma/EFS/EnigmaEFS.cs
@@ -1,4 +1,8 @@
+using System;
 using System.IO;
+using System.Security.Cryptography;
+using Enigma.CryptedFileParser;
+using Enigma.Models;
 
 namespace Enigma.EFS
 {
@@ -10,9 +14,12 @@ namespace Enigma.EFS
         private readonly string mountLocation = @"D:";
         private readonly string rootDir = @"D:\EnigmaEFS";
         private readonly string sharedDir = @"D:\EnigmaEFS\Shared";
+        private readonly UserInformation currentUser;
         public long AvailableFreeSpace { get; set; }
-        public EnigmaEfs()
+        public EnigmaEfs(UserInformation user)
         {
+            currentUser = user;
+
             // EFS "mount"
             if (Directory.Exists(mountLocation))
             {
@@ -46,6 +53,43 @@ namespace Enigma.EFS
             {
                 return false;
             }
+        }
+
+        public void Upload(string locationOfFile, string locationOnEfs, string algorithmNameSignature, string hashAlgorithmName)
+        {
+            var fileSize = new FileInfo(locationOfFile).Length;
+
+            if(fileSize > 4_000_000_000)
+            {
+                throw new Exception("File can't be larger than 4 GB.");
+            }
+
+            if (CanItBeStored(fileSize))
+            {
+                var fullFileName = locationOfFile.Substring(locationOfFile.LastIndexOf('\\') + 1);
+                var originalFile = new OriginalFile(File.ReadAllBytes(locationOfFile), fullFileName);
+
+                var encryptedFile = new EncryptedFile(fullFileName, (uint)currentUser.user.Id, algorithmNameSignature, hashAlgorithmName, currentUser.PublicKey, currentUser.PrivateKey);
+                var encryptedFileRaw = encryptedFile.Encrypt(originalFile, currentUser.user.Id, currentUser.PrivateKey);
+
+                using var stream = new FileStream(locationOnEfs + "\\" + encryptedFile.GetEncryptedFileFullName(), FileMode.Create);
+                using var writter = new BinaryWriter(stream);
+                writter.Write(encryptedFileRaw);
+            }
+            else
+            {
+                throw new Exception("Insufficient storage available.");
+            }
+        }
+
+        public void Download(string locationOnEfs, string locationOnFs, RSAParameters ownerPublicKey)
+        {
+            var encryptedFile = new EncryptedFile();
+            var originalFile = encryptedFile.Decrypt(File.ReadAllBytes(locationOnEfs), currentUser.user.Id, currentUser.PrivateKey, ownerPublicKey);
+
+            using var stream = new FileStream(locationOnFs + "\\" + originalFile.GetOriginalFileFullName(), FileMode.Create);
+            using var writter = new BinaryWriter(stream);
+            writter.Write(originalFile.FileContent);
         }
     }
 }
