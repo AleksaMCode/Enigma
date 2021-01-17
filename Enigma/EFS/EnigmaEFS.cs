1065758560d744061c04d8d05c5a91e5ba523d46
Aleksa Majkic <aleksamcode@gmail.com>
2021-01-17 15:32:06 +0100

    RemoveTempFiles method added; XML documentation comment added; class updated/bugs removed.

--- Enigma/EFS/EnigmaEFS.cs
+++ Enigma/EFS/EnigmaEFS.cs
@@ -16,8 +16,17 @@ namespace Enigma.EFS
         private readonly string mountLocation = @"D:";
         private readonly string rootDir = @"D:\EnigmaEFS";
         private readonly string sharedDir = @"D:\EnigmaEFS\Shared";
+
+        /// <summary>
+        /// Information about the currently logged in user.
+        /// </summary>
         private readonly UserInformation currentUser;
-        public long AvailableFreeSpace { get; set; }
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnigmaEfs"/> class with the specified user information.
+        /// </summary>
+        /// <param name="user">Information about the currently logged in user from the database.</param>
         public EnigmaEfs(UserInformation user)
         {
             currentUser = user;
@@ -40,14 +49,17 @@ namespace Enigma.EFS
             {
                 // error handle
             }
-            // set AvailableFreeSpace value
-            AvailableFreeSpace = new DriveInfo(mountLocation).AvailableFreeSpace;
         }
 
+        /// <summary>
+        /// Check if the sufficient storage space is available to store a new file.
+        /// </summary>
+        /// <param name="size">Size of the file that is being uplaoded to EFS.</param>
+        /// <returns></returns>
         public bool CanItBeStored(long size)
         {
-            AvailableFreeSpace = new DriveInfo(mountLocation).AvailableFreeSpace;
-            if (size < AvailableFreeSpace)
+            var availableFreeSpace = new DriveInfo(mountLocation).AvailableFreeSpace;
+            if (size < availableFreeSpace)
             {
                 return true;
             }
@@ -57,9 +69,36 @@ namespace Enigma.EFS
             }
         }
 
-        public void Upload(string locationOfFile, string locationOnEfs, string algorithmNameSignature, string hashAlgorithmName)
+        /// <summary>
+        /// Check if the sufficient storage space on specified drive is available to store a new file.
+        /// </summary>
+        /// <param name="size">Size of the file that is being downloaded to file system.</param>
+        /// <param name="driveName">Name of the drive where file will be stored.</param>
+        /// <returns></returns>
+        public bool CanItBeStored(long size, string driveName)
         {
-            var fileSize = new FileInfo(locationOfFile).Length;
+            var availableFreeSpace = new DriveInfo(driveName).AvailableFreeSpace;
+            if (size < availableFreeSpace)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Uplaods selected file. File is first encrypted after which is stored on the specified path on encrypted file system.
+        /// </summary>
+        /// <param name="path">The fully qualified name of the new file.</param>
+        /// <param name="pathOnEfs">Path on the encrypted file system where the file will be stored.</param>
+        /// <param name="algorithmNameSignature">Name of the algorithm used for file encryption.</param>
+        /// <param name="hashAlgorithmName">Name of the hashing algorithm used to create a file signature.</param>
+        public void Upload(string path, string pathOnEfs, string algorithmNameSignature, string hashAlgorithmName)
+        {
+            var fileSize = new FileInfo(path).Length;
 
             if (fileSize > 4_000_000_000)
             {
@@ -68,65 +107,117 @@ namespace Enigma.EFS
 
             if (CanItBeStored(fileSize))
             {
-                var fullFileName = locationOfFile.Substring(locationOfFile.LastIndexOf('\\') + 1);
-                var originalFile = new OriginalFile(File.ReadAllBytes(locationOfFile), fullFileName);
+                var fullFileName = path.Substring(path.LastIndexOf('\\') + 1);
+                var originalFile = new OriginalFile(File.ReadAllBytes(path), fullFileName);
 
                 var encryptedFile = new EncryptedFile(fullFileName, (uint)currentUser.user.Id, algorithmNameSignature, hashAlgorithmName, currentUser.PublicKey, currentUser.PrivateKey);
                 var encryptedFileRaw = encryptedFile.Encrypt(originalFile, currentUser.user.Id, currentUser.PrivateKey);
 
-                using var stream = new FileStream(locationOnEfs + "\\" + encryptedFile.GetEncryptedFileFullName(), FileMode.Create);
-                using var writter = new BinaryWriter(stream);
-                writter.Write(encryptedFileRaw);
+                if (CanItBeStored(encryptedFileRaw.Length))
+                {
+                    using var stream = new FileStream(pathOnEfs + "\\" + encryptedFile.GetEncryptedFileFullName(), FileMode.Create);
+                    using var writter = new BinaryWriter(stream);
+                    writter.Write(encryptedFileRaw);
+                }
+                else
+                {
+                    throw new Exception("Insufficient storage available. File can't be uploaded.");
+                }
             }
             else
             {
-                throw new Exception("Insufficient storage available.");
+                throw new Exception("Insufficient storage available. File can't be uploaded.");
             }
         }
 
-        public void Download(string locationOnEfs, string locationOnFs, RSAParameters ownerPublicKey)
+        /// <summary>
+        /// Downlods selected encrypted file. File is first decrypted after which is stored on the specified path on file system.
+        /// </summary>
+        /// <param name="pathOnEfs">The name of the file to downloaded.</param>
+        /// <param name="pathOnFs">Path on the file system where the file will be stored.</param>
+        /// <param name="ownerPublicKey">Public RSA key from the file owner used to check files signature.</param>
+        public void Download(string pathOnEfs, string pathOnFs, RSAParameters ownerPublicKey)
         {
             var encryptedFile = new EncryptedFile();
-            var originalFile = encryptedFile.Decrypt(File.ReadAllBytes(locationOnEfs), currentUser.user.Id, currentUser.PrivateKey, ownerPublicKey);
+            var originalFile = encryptedFile.Decrypt(File.ReadAllBytes(pathOnEfs), currentUser.user.Id, currentUser.PrivateKey, ownerPublicKey);
 
-            using var stream = new FileStream(locationOnFs + "\\" + originalFile.GetOriginalFileFullName(), FileMode.Create);
-            using var writter = new BinaryWriter(stream);
-            writter.Write(originalFile.FileContent);
+            if (CanItBeStored(originalFile.FileContent.Length, pathOnFs.Substring(0, 2)))
+            {
+                using var stream = new FileStream(pathOnFs + "\\" + originalFile.GetOriginalFileFullName(), FileMode.Create);
+                using var writter = new BinaryWriter(stream);
+                writter.Write(originalFile.FileContent);
+            }
+            else
+            {
+                throw new Exception("Insufficient storage available. File can't be downloaded.");
+            }
         }
 
-        public void CreateTxtFile(string text, string locationOnFs, string fileName)
+        /// <summary>
+        /// Creates a new <em>.txt</em> file on file system.
+        /// </summary>
+        /// <param name="text">Content of the file.</param>
+        /// <param name="pathOnFs">Path on the file system where the file will be stored.</param>
+        /// <param name="fileName">Name of the .txt file.</param>
+        public void CreateTxtFile(string text, string pathOnFs, string fileName)
         {
             var textFile = new OriginalFile(Encoding.ASCII.GetBytes(text), fileName + ".txt");
 
-            using var stream = new FileStream(locationOnFs + "\\" + textFile.GetOriginalFileFullName(), FileMode.Create);
+            using var stream = new FileStream(pathOnFs + "\\" + textFile.GetOriginalFileFullName(), FileMode.Create);
             using var writter = new BinaryWriter(stream);
             writter.Write(textFile.FileContent);
         }
 
-        public void EditTxtFile(string text, string locationOnFs)
+        /// <summary>
+        /// Edits an existing <em>.txt</em> file on file system by overwriting the original file.
+        /// </summary>
+        /// <param name="text">Content from the .txt file.</param>
+        /// <param name="pathOnFs">Path on the file system where file is stored which includes files name.</param>
+        public void EditTxtFile(string text, string pathOnFs)
         {
-            var textFile = new OriginalFile(Encoding.ASCII.GetBytes(text), locationOnFs.Substring(locationOnFs.LastIndexOf('\\') + 1));
+            var fileName = pathOnFs.Substring(pathOnFs.LastIndexOf('\\') + 1);
+            if (fileName.Substring(fileName.LastIndexOf('.') + 1).Equals("txt"))
+            {
+                var textFile = new OriginalFile(Encoding.ASCII.GetBytes(text), pathOnFs.Substring(pathOnFs.LastIndexOf('\\') + 1));
 
-            using var stream = new FileStream(locationOnFs + "\\" + textFile.GetOriginalFileFullName(), FileMode.Create);
-            using var writter = new BinaryWriter(stream);
-            writter.Write(textFile.FileContent);
+                using var stream = new FileStream(pathOnFs + "\\" + textFile.GetOriginalFileFullName(), FileMode.Create);
+                using var writter = new BinaryWriter(stream);
+                writter.Write(textFile.FileContent);
+            }
+            else
+            {
+                throw new Exception("File you are trying to edit isn't a .txt file");
+            }
         }
 
+        /// <summary>
+        /// Deletes the specified file.
+        /// </summary>
+        /// <param name="path">The name of the file to be deleted. Wildcard characters are not supported.</param>
         public void DeleteFile(string path)
         {
             File.Delete(path);
         }
 
+        /// <summary>
+        /// Deletes the specified directory and and any subdirectories and files in the directory.
+        /// </summary>
+        /// <param name="path">The name of the directory to remove.</param>
         public void DeleteDirectory(string path)
         {
             Directory.Delete(path, true);
         }
 
-        public void ReadFile(string locationOnEfs, RSAParameters ownerPublicKey, out string tempFilePath)
+        /// <summary>
+        /// Reads users encrypted file. File is first decrypted and stored as a temporary file in temporary folder after which is opened in a default app for that file set on the computer. 
+        /// </summary>
+        /// <param name="pathOnEfs">The name of the encrypted file including files path and encrypted name with .at extension.</param>
+        /// <param name="ownerPublicKey">Public RSA key from the file owner used to check files signature.</param>
+        public void ReadFile(string pathOnEfs, RSAParameters ownerPublicKey)
         {
             var encryptedFile = new EncryptedFile();
-            var originalFile = encryptedFile.Decrypt(File.ReadAllBytes(locationOnEfs), currentUser.user.Id, currentUser.PrivateKey, ownerPublicKey);
-            tempFilePath = Path.GetTempPath() + "Enigma-" + Guid.NewGuid().ToString() + "." + originalFile.FileExtension;
+            var originalFile = encryptedFile.Decrypt(File.ReadAllBytes(pathOnEfs), currentUser.user.Id, currentUser.PrivateKey, ownerPublicKey);
+            var tempFilePath = Path.GetTempPath() + "Enigma-" + Guid.NewGuid().ToString() + "." + originalFile.FileExtension;
 
             using (var stream = new FileStream(tempFilePath, FileMode.Create))
             {
@@ -136,6 +227,18 @@ namespace Enigma.EFS
 
             var startInfo = new ProcessStartInfo(tempFilePath);
             Process.Start(startInfo);
+        }
+
+        /// <summary>
+        /// Removes all temporary files created while reading encrypted files. This method is called when application is closed.
+        /// </summary>
+        public void RemoveTempFiles()
+        {
+            var filesToDelete = Directory.GetFiles(Path.GetTempPath(), "Enigma-*");
+            foreach(var fileName in filesToDelete)
+            {
+                File.Delete(fileName);
+            }
         }
     }
 }
