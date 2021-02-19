f8a522177b7973d03311bcb3be50b61bd8fb5558
Aleksa Majkic <aleksamcode@gmail.com>
2021-02-20 00:39:09 +0100

    TxtFormData class added.

---
+++ Enigma.Wpf/Forms/Data/TxtFormData.cs
@@ -0,0 +1,9 @@
+namespace Enigma.Wpf.Forms.Data
+{
+    public class TxtFormData
+    {
+        public string Text { get; set; }
+        public string AlgorithmIdentifier { get; set; }
+        public string HashIdentifier { get; set; }
+    }
+}
